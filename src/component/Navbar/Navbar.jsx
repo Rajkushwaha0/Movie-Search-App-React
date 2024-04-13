d19921de43a2063e8976db68d3f2bd20f4f1c8b0
Raj kushwaha <87632065+Rajkushwaha0@users.noreply.github.com>
2024-04-13 12:24:27 +0530

    Added navbar styling and Error page

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import "./navbar.css";
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <Link to={"/"} className="title">
-        MovieCom
-      </Link>
+    <div className="navbar-wrapper">
+      <div>
+        <Link to={"/"} className="title">
+          Movie Com
+        </Link>
+      </div>
+      <div className="search-bar">
+        <input type="text" placeholder="What movie are thinking about..." />
+      </div>
+      <div>Theme</div>
     </div>
   );
 }
